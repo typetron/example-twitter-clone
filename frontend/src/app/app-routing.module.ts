f0bde49544af29f6122f16d5778319898a7482e7
ionel <ionel.cristian.lupu@gmail.com>
2020-10-07 09:47:32 +0300

    ng zorro 10

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,21 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './auth.guard';
 
-const routes: Routes = [
-    {
-        path: '',
-        canActivate: [AuthGuard],
-        loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
-    },
-    {
-        path: '',
-        loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
-    }
-];
+const routes: Routes = [];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {}
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,3 @@
-import '@Typetron/Support';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
@@ -6,8 +5,8 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
-    enableProdMode();
+  enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-    .catch(err => console.error(err));
+  .catch(err => console.error(err));
